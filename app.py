84954a4c8927b42432bd1f55e1e8d5ca6528b6e9
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-10-15 12:24:21 -0400

    app additions

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 #
-
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find()
+    return render_template("index.html", mars=mars)
+#
+@app.route("/scrape")
+def scraper():
+    mars = mongo.db.mars
+    mars_data = scrape.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+#
 if __name__ == "__main__":
     app.run(debug=True)
